823010aeff869d0fa2a1023b17bd804e43fe8630
Bas Moret <bas.moret@student.hu.nl>
2021-03-26 16:26:06 +0100

    Initial setup for the controller and service layer

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/presentation/GameDTOResponse.java
@@ -0,0 +1,9 @@
+package nl.hu.cisq1.lingo.trainer.presentation;
+
+import nl.hu.cisq1.lingo.trainer.domain.Game;
+
+public class GameDTOResponse {
+    public GameDTOResponse(Game game) {
+
+    }
+}
