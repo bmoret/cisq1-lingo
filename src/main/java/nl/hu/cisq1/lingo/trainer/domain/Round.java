c474013e3764bd222095060ce9b722d4bebf8297
Bas Moret <bas.moret@student.hu.nl>
2021-03-26 13:19:46 +0100

    Added and tested Round

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Round.java
@@ -0,0 +1,97 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+@Entity
+public class Round {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "round_id")
+    private Long id;
+    private String wordToGuess;
+    @Enumerated
+    private State state;
+    @ElementCollection
+    private List<String> hint = new ArrayList<>();
+
+    @OneToMany(mappedBy = "round")
+    private List<Feedback> guesses = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    public Round() {}
+
+    public Round(String wordToGuess) {
+        this.wordToGuess = wordToGuess;
+        this.state = State.PLAYING;
+    }
+
+    public Feedback makeGuess(String guess) {
+        if (state.equals(State.PLAYING)) {
+            Feedback feedback = getFeedback(guess);
+            if (feedback.isWordValid()) {
+                guesses.add(feedback);
+                hint = feedback.giveHint(hint, wordToGuess);
+            }
+            if (feedback.isWordGuessed()) {
+                state = State.WON;
+            } else if (guesses.size() == 5) {
+                state = State.LOST;
+            }
+            return feedback;
+        }
+        throw new IllegalArgumentException("Game is finished, no new round started");
+
+    }
+
+    public Feedback getFeedback(String guess) {
+        List<String> guessTemp = new ArrayList<>();
+        List<String> wordToGuessTemp = new ArrayList<>();
+        List<Mark> mark = new ArrayList<>();
+
+        if (guess.length() == getWordToGuess().length()) {
+            for (int x = 0; x < guess.length(); x++) {
+                guessTemp.add(String.valueOf(guess.charAt(x)));
+                wordToGuessTemp.add(String.valueOf(wordToGuess.charAt(x)));
+            }
+        }
+
+        Pattern p = Pattern.compile("[^a-zA-Z0-9]");
+        for (int x = 0; x < guess.length(); x++) {
+            if (guess.length() != getWordToGuess().length()) {
+                mark.add(Mark.INVALID);
+            } else if (p.matcher(guess).find()) {
+                mark.add(Mark.INVALID);
+            } else if (guess.charAt(x) == wordToGuess.charAt(x)) {
+                mark.add(Mark.CORRECT);
+                guessTemp.remove(String.valueOf(guess.charAt(x)));
+                wordToGuessTemp.remove(String.valueOf(wordToGuess.charAt(x)));
+            }
+        }
+        for (int x = 0; x < guess.length(); x++) {
+            if (guess.length() == getWordToGuess().length() && !p.matcher(guess).find() && guess.charAt(x) != wordToGuess.charAt(x)) {
+                if (wordToGuessTemp.contains(guessTemp.get(0))) {
+                    mark.add(x, Mark.PRESENT);
+                    guessTemp.remove(String.valueOf(guess.charAt(x)));
+                    wordToGuessTemp.remove(String.valueOf(wordToGuess.charAt(x)));
+                } else {
+                    mark.add(x, Mark.ABSENT);
+                    guessTemp.remove(String.valueOf(guess.charAt(x)));
+                }
+            }
+        }
+        return new Feedback(guess, mark);
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public String getWordToGuess() {
+        return wordToGuess;
+    }
+}
---
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/RoundTest.java
@@ -0,0 +1,163 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import nl.hu.cisq1.lingo.trainer.exception.InvalidFeedbackException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoundTest {
+
+    @Test
+    @DisplayName("Testing if a round is won, after a correct guess")
+    void gameIsWonAfterOneGuess() {
+        Round round = new Round("woord");
+
+        round.makeGuess("woord");
+
+        assertEquals(State.WON, round.getState());
+    }
+
+    @Test
+    @DisplayName("Testing if a round is active, after a incorrect guess")
+    void gameIsPlayingAfterOneGuess() {
+        Round round = new Round("woord");
+
+        round.makeGuess("boord");
+
+        assertEquals(State.PLAYING, round.getState());
+    }
+
+    @Test
+    @DisplayName("Testing if a round is won, after five guesses")
+    void gameIsWonAfterFiveGuesses() {
+        Round round = new Round("woord");
+
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("woord");
+
+        assertEquals(State.WON, round.getState());
+    }
+
+    @Test
+    @DisplayName("Testing if a round is won, even after guessing more than five times because of invalid guesses")
+    void gameIsWonAfterInvalidGuesses() {
+        Round round = new Round("woord");
+
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("asdoor");
+        round.makeGuess("@door");
+        round.makeGuess("woord");
+
+        assertEquals(State.WON, round.getState());
+    }
+
+    @Test
+    @DisplayName("Testing if a round is lost, after five wrong guesses")
+    void gameIsLostAfterFiveGuesses() {
+        Round round = new Round("woord");
+
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+
+        assertEquals(State.LOST, round.getState());
+    }
+
+    @Test
+    @DisplayName("Testing if a exception is thrown, after guessing again, after losing")
+    void throwsExceptionWhenGuessingAfterLoss() {
+        Round round = new Round("woord");
+
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+        round.makeGuess("doord");
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> round.makeGuess("doord")
+        );
+    }
+
+    @Test
+    @DisplayName("Testing if a exception is thrown, after guessing again, after winning")
+    void throwsExceptionWhenGuessingAfterWin() {
+        Round round = new Round("woord");
+
+        round.makeGuess("woord");
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> round.makeGuess("woord")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideListsForMakeGuess")
+    @DisplayName("Testing if feedback is made correctly")
+    void getFeedback(String guess, String wordToGuess, Feedback expected) {
+        Round round = new Round(wordToGuess);
+
+        assertEquals(expected, round.getFeedback(guess));
+    }
+
+    private static Stream<Arguments> provideListsForMakeGuess() {
+        String a1 = "boord";
+        String a2 = "woord";
+        List<Mark> aMark = Arrays.asList(Mark.ABSENT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT);
+        Feedback a3 = new Feedback(a1, aMark);
+
+        String b1 = "ddddd";
+        String b2 = "woord";
+        List<Mark> bMark = Arrays.asList(Mark.ABSENT,Mark.ABSENT,Mark.ABSENT,Mark.ABSENT,Mark.CORRECT);
+        Feedback b3 = new Feedback(b1, bMark);
+
+        String c1 = "bosod";
+        String c2 = "woord";
+        List<Mark> cMark = Arrays.asList(Mark.ABSENT,Mark.CORRECT,Mark.ABSENT,Mark.PRESENT,Mark.CORRECT);
+        Feedback c3 = new Feedback(c1, cMark);
+
+        String d1 = "aaaao";
+        String d2 = "woord";
+        List<Mark> dMark = Arrays.asList(Mark.ABSENT,Mark.ABSENT,Mark.ABSENT,Mark.ABSENT,Mark.PRESENT);
+        Feedback d3 = new Feedback(d1, dMark);
+
+        String e1 = "boorde";
+        String e2 = "woord";
+        List<Mark> eMark = Arrays.asList(Mark.INVALID,Mark.INVALID,Mark.INVALID,Mark.INVALID,Mark.INVALID,Mark.INVALID);
+        Feedback e3 = new Feedback(e1, eMark);
+
+        String f1 = "aaa@o";
+        String f2 = "woord";
+        List<Mark> fMark = Arrays.asList(Mark.INVALID,Mark.INVALID,Mark.INVALID,Mark.INVALID,Mark.INVALID);
+        Feedback f3 = new Feedback(f1, fMark);
+
+
+        return Stream.of(
+                Arguments.of(a1, a2, a3),
+                Arguments.of(b1, b2, b3),
+                Arguments.of(c1, c2, c3),
+                Arguments.of(d1, d2, d3),
+                Arguments.of(e1, e2, e3),
+                Arguments.of(f1, f2, f3)
+        );
+    }
+}
