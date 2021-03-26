128c2019b9a92810ddd24c649587558ab447c8c6
Bas Moret <bas.moret@student.hu.nl>
2021-03-26 16:25:27 +0100

    Added Game and GameTest

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
@@ -0,0 +1,70 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "game_id")
+    private Long id;
+    private int score;
+    private Boolean isFinished = false;
+    @OneToMany(mappedBy = "game")
+    private List<Round> rounds = new ArrayList<>();;
+
+    public Game() {}
+
+    public Round newRound(String wordToGuess) {
+        if (!isFinished()) {
+            List<Round> activeRounds = this.rounds.stream().filter(e -> e.getState() == State.PLAYING).collect(Collectors.toList());
+            if (activeRounds.size() == 0) {
+                Round round = new Round(wordToGuess);
+                rounds.add(round);
+                return round;
+            }
+            throw new IllegalArgumentException("Can't start a new round while other rounds are active");
+        }
+        throw new IllegalArgumentException("Game is finished, no new round started");
+    }
+
+    public Round makeGuess(String guess) {
+        if (!isFinished()) {
+            List<Round> activeRounds = this.rounds.stream().filter(e -> e.getState() == State.PLAYING).collect(Collectors.toList());
+            if (activeRounds.size() == 1) {
+                Round activeRound = activeRounds.get(0);
+                activeRound.makeGuess(guess);
+                if (activeRound.getState().equals(State.LOST)) {
+                    isFinished = true;
+                } if (activeRound.getState().equals(State.WON)) {
+                    score += 1;
+                }
+                return activeRound;
+            }
+            throw new IllegalArgumentException("No valid round found");
+        }
+        throw new IllegalArgumentException("Game is finished, no guess made");
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public List<Round> getRounds() {
+        return rounds;
+    }
+
+    public Boolean isFinished() {
+        return isFinished;
+    }
+
+    public int getWordLengthForNextRound() {
+        if (this.rounds.isEmpty()) {
+            return 5;
+        }
+        return rounds.size()%3+5;
+    }
+}
---
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/GameTest.java
@@ -0,0 +1,205 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    @DisplayName("Tests if a new round is added to a game")
+    void newRound() {
+        Game game = new Game();
+
+        Round round = game.newRound("woord");
+
+        assertTrue(game.getRounds().size() == 1);
+        assertTrue(game.getRounds().contains(round));
+    }
+
+    @Test
+    @DisplayName("Tests if a new round can be started if the game is won")
+    void newRoundWhenRoundIsWon() {
+        Game game = new Game();
+
+        Round round1 = game.newRound("woord");
+
+        game.makeGuess("woord");
+
+        Round round2 = game.newRound("woord");
+
+        assertTrue(game.getRounds().size() == 2);
+        assertTrue(game.getRounds().contains(round1));
+        assertTrue(game.getRounds().contains(round2));
+    }
+
+
+    @Test
+    @DisplayName("Tests if a new round can't be started if there is an active round")
+    void newRoundWhenOnIsAlreadyActive() {
+        Game game = new Game();
+
+        game.newRound("woord");
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> game.newRound("woord")
+        );
+    }
+
+    @Test
+    @DisplayName("Tests if a new round can't be started if the game is over")
+    void newRoundWhenGameIsOver() {
+        Game game = new Game();
+
+        game.newRound("woord");
+
+        game.makeGuess("doord");
+        game.makeGuess("doord");
+        game.makeGuess("doord");
+        game.makeGuess("doord");
+        game.makeGuess("doord");
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> game.newRound("woord")
+        );
+
+    }
+
+    @Test
+    @DisplayName("Tests if a guess is added to a round")
+    void makeGuess() {
+        Game game = new Game();
+
+        Round round = game.newRound("woord");
+
+        assertEquals(0, round.getGuesses().size());
+
+        game.makeGuess("doord");
+
+        assertEquals(1, round.getGuesses().size());
+    }
+
+    @Test
+    @DisplayName("Tests if a guess can't be made when no round is active")
+    void makeGuessWhenNoRoundHasStarted() {
+        Game game = new Game();
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> game.makeGuess("woord")
+        );
+
+    }
+
+    @Test
+    @DisplayName("Tests if a guess can't be made when the game is over")
+    void makeGuessWhenGameIsOver() {
+        Game game = new Game();
+
+        game.newRound("woord");
+
+        game.makeGuess("doord");
+        game.makeGuess("doord");
+        game.makeGuess("doord");
+        game.makeGuess("doord");
+        game.makeGuess("doord");
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> game.makeGuess("woord")
+        );
+
+    }
+
+    @Test
+    @DisplayName("Tests if the games score is increased after guessing the word")
+    void scoreIsIncreasedWhenRoundIsWon() {
+        Game game = new Game();
+
+        assertEquals(0, game.getScore());
+
+        game.newRound("woord");
+        game.makeGuess("woord");
+
+        assertEquals(1, game.getScore());
+
+        game.newRound("woord");
+        game.makeGuess("boord");
+        game.makeGuess("woord");
+
+        assertEquals(2, game.getScore());
+
+    }
+
+    @Test
+    @DisplayName("Tests if the games score is not increased after not guessing the word")
+    void scoreIsNotIncreasedWhenWordIsNotGuessed() {
+        Game game = new Game();
+
+        assertEquals(0, game.getScore());
+
+        game.newRound("woord");
+        game.makeGuess("woord");
+
+        assertEquals(1, game.getScore());
+
+        game.newRound("woord");
+        game.makeGuess("boord");
+
+        assertEquals(1, game.getScore());
+
+    }
+
+    @Test
+    @DisplayName("Tests if the games score is increased after losing the game")
+    void scoreIsNotIncreasedWhenRoundIsLost() {
+        Game game = new Game();
+
+        assertEquals(0, game.getScore());
+
+        game.newRound("woord");
+        game.makeGuess("woord");
+
+        assertEquals(1, game.getScore());
+
+        game.newRound("woord");
+        game.makeGuess("boord");
+        game.makeGuess("boord");
+        game.makeGuess("boord");
+        game.makeGuess("boord");
+        game.makeGuess("boord");
+
+        assertEquals(1, game.getScore());
+
+    }
+
+    @Test
+    @DisplayName("Tests if the word length is returned correctly at the start of the game")
+    void wordLengthStart() {
+        Game game = new Game();
+
+//        assertEquals(7, game.getWordLength());
+    }
+
+    @Test
+    @DisplayName("Tests if the word length is returned correctly during round 1 of the game")
+    void wordLengthRound1() {
+        Game game = new Game();
+
+        for (int x = 0; x < 3; x++) {
+            assertEquals(5, game.getWordLengthForNextRound());
+            game.newRound("woord");
+            game.makeGuess("woord");
+            assertEquals(6, game.getWordLengthForNextRound());
+            game.newRound("wooord");
+            game.makeGuess("wooord");
+            assertEquals(7, game.getWordLengthForNextRound());
+            game.newRound("woooord");
+            game.makeGuess("woooord");
+            assertEquals(5, game.getWordLengthForNextRound());
+        }
+
+    }
+}
